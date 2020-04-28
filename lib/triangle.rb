0c6c55c15b0fa6e07708e071350329fc8edfd10b
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-28 03:56:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
   end
 
   def kind
-    if @lengthA = @lengthB && @lengthA = @lengthC
+    if @lengthA == @lengthB && @lengthA == @lengthC
       return :equilateral
     end
     if @lengthA != @lengthB && @lengthB = @lengthC && @lengthA = @lengthC
