9e77d711609c4103fd6eaee470700640cd079c4b
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-28 03:38:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,10 @@ class Triangle
 
   attr_accessor :equilateral, :isosceles, :scalene
 
-  def initialize()
+  def initialize(lengthA, lengthB, lengthC)
+    @lengthA = lengthA
+    @lengthB = lengthB
+    @lengthC = lengthC
 
   end
 end
