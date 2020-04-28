4ccd1d52cc0d197da1f6a7a77d9677cf31c0f328
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-28 04:45:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,17 +7,20 @@ class Triangle
   end
 
   def kind
+    if @lengthA <= 0 || @lengthB <=0 || @lengthC <= 0
+        raise TriangleError
+    end
+    
     if @lengthA == @lengthB && @lengthA == @lengthC
       return :equilateral
     end
     if @lengthA == @lengthB || @lengthB == @lengthC || @lengthC == @lengthA
       return :isosceles
     end
-      :scalene
-    end
+    :scalene
+  end
 
-    class TriangleError < StandardError
-      # triangle error code
-    end
+  class TriangleError < StandardError
+    
   end
-  end
+end
