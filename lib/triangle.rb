a3d062f2fb8f91e6a247b0073e141cdbf7782a26
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-28 03:46:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,13 @@
 class Triangle
 
   def initialize(lengthA, lengthB, lengthC)
-
+    @lengthA = lengthA
+    @lengthB = lengthB
+    @lengthC = lengthC
   end
 
   def kind
-    if lengthA = lengthB && lengthA = lengthC
+    if @lengthA = @lengthB && @lengthA = @lengthC
       return :equilateral
     end
   end
