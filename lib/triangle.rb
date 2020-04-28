9486a980a02066bc68e6d0b2d9675e157300b3e7
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-28 04:49:24 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+
+  def initialize(lengthA, lengthB, lengthC)
+    @lengthA = lengthA
+    @lengthB = lengthB
+    @lengthC = lengthC
+  end
+
+  def kind
+    if @lengthA <= 0 || @lengthB <=0 || @lengthC <= 0 || (@lengthA+@lengthB <= @lengthC) || (@lengthC+@lengthB <= @lengthA) || (@lengthC+@lengthA <= @lengthB)
+        raise TriangleError
+    end
+
+    if @lengthA == @lengthB && @lengthA == @lengthC
+      return :equilateral
+    end
+    if @lengthA == @lengthB || @lengthB == @lengthC || @lengthC == @lengthA
+      return :isosceles
+    end
+    :scalene
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
